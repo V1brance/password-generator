3064ba4e2de35562a878419b7f2b0ad649bc621c
Neon Riot <sbrookshire99@gmail.com>
2021-04-21 22:32:29 -0500

    Huge commit, completely built out the js file in one go, added functions to select random characters from the arrays selected by the users and returns the concatenated string

--- Develop/script.js
+++ Develop/script.js
@@ -14,8 +14,102 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   // Declare some variables to start, the order is important for the rest of the function
+  var newPassword = "";
   var charTypes = ["lowercase", "uppercase", "numeric", "special"];
+  var selectedTypes = [];
+  var trues = 0;
   var passwordLength = 0;
+  var uppercase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var lowercase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "e",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var special = [
+    " ",
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "}",
+    "|",
+    "~",
+  ];
 
   // Utilize a while loop to prevent input outside the parameters of 8-128
   while (passwordLength < 8 || passwordLength > 128) {
@@ -24,7 +118,8 @@ function generatePassword() {
     );
   }
 
-  // A for loop iterates through the charTypes array and changes the values to true or false depending on user input of desired char Types
+  // An empty aray is populated with the selections made by the user during this for loop, if a user hits y on a character
+  // type then a string of that character type is added to the array
   for (i = 0; i < charTypes.length; i++) {
     var charSelect = prompt(
       "Would you like " +
@@ -32,9 +127,39 @@ function generatePassword() {
         " characters in your password? Enter y/n:"
     );
     if (charSelect == "y") {
-      charTypes[i] = true;
-    } else {
-      charTypes[i] = false;
+      selectedTypes.push(charTypes[i]);
+      trues++;
     }
   }
+
+  //A var called trues validates that at least one option has been selected for the password
+  if (trues == 0) {
+    return "You did not select any text options, please try again and answer yes to at least one character option";
+  }
+
+  console.log(selectedTypes);
+
+  //Utilize a while loop to add random characters from random arrays selected from the user input and add these new characters
+  //to the new password
+  while (newPassword.length < passwordLength) {
+    var nextChar = "";
+    var nextCharType =
+      selectedTypes[Math.floor(Math.random() * selectedTypes.length)];
+
+    console.log(nextCharType);
+
+    if (nextCharType == "lowercase") {
+      nextChar = lowercase[Math.floor(Math.random() * lowercase.length)];
+    } else if (nextCharType == "uppercase") {
+      nextChar = uppercase[Math.floor(Math.random() * uppercase.length)];
+    } else if (nextCharType == "numeric") {
+      nextChar = numeric[Math.floor(Math.random() * numeric.length)];
+    } else if (nextCharType == "special") {
+      nextChar = special[Math.floor(Math.random() * special.length)];
+    }
+
+    newPassword = newPassword.concat(nextChar);
+  }
+
+  return newPassword;
 }
